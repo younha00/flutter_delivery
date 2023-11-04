2f19fb6b2a7c8338d00744e4faaa0d81e9d167f3
younha00 <ayeon0@office.skhu.ac.kr>
2023-11-04 15:31:50 +0900

    feat: 기초 로그인

---
+++ lib/common/view/root_tab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RootTap extends StatelessWidget {
+  const RootTap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('root tap'),
+      ),
+    );
+  }
+}
--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,14 +1,35 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_delivery/common/component/custom_text_form_field.dart';
 import 'package:flutter_delivery/common/const/colors.dart';
 import 'package:flutter_delivery/common/layout/default_layout.dart';
+import 'package:flutter_delivery/common/view/root_tab.dart';
 import 'package:gap/gap.dart';
+import 'package:dio/dio.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
+
+  @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+
+    // localhost
+    const emulatorIp = '10.0.2.2:3000';
+    const simulatorIp = '127.0.0.1:3000';
+
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
       child: SingleChildScrollView(
         // 키보드 내리기
@@ -30,17 +51,44 @@ class LoginScreen extends StatelessWidget {
                 ),
                 CustomTextFormField(
                   hintText: '이메일을 입력해주세요',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    username = value;
+                  },
                 ),
                 const Gap(16),
                 CustomTextFormField(
                   hintText: '비밀번호를 입력해주세요',
                   obscureText: true,
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    password = value;
+                  },
                 ),
                 const Gap(16),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    // ID:비밀번호
+                    final rawString = '$username:$password';
+
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+                    String token = stringToBase64.encode(rawString);
+
+                    final response = await dio.post(
+                      'http://$ip/auth/login',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Basic $token',
+                        },
+                      ),
+                    );
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => const RootTap(),
+                      ),
+                    );
+
+                    print(response.data);
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: PRIMARY_COLOR,
                     shape: RoundedRectangleBorder(
@@ -55,7 +103,20 @@ class LoginScreen extends StatelessWidget {
                   style: TextButton.styleFrom(
                     foregroundColor: Colors.black,
                   ),
-                  onPressed: () {},
+                  onPressed: () async {
+                    const refreshToken =
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY5OTA3ODQwMiwiZXhwIjoxNjk5MTY0ODAyfQ.OM-p-t2U6eKikd8bgmx1l1lqS8Jls7pPbZhjH_8eILw';
+                    final response = await dio.post(
+                      'http://$ip/auth/token',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Bearer $refreshToken',
+                        },
+                      ),
+                    );
+
+                    print(response.data);
+                  },
                   child: const Text(
                     "회원가입",
                   ),
